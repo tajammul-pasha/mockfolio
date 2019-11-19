57dbdb67578e77537c96776d8b5ce534baf492c2
tajammul-pasha <tajammul.pasha@xelpmoc.in>
2019-11-19 11:40:42 +0530

    added toggle to toggle the visibility of the sidebar navigator

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showNavigator() {
+  document.getElementById("side-bar").style.display = "block";
+}
+
+function hideNavigator() {
+  document.getElementById("side-bar").style.display = "none";
+}
